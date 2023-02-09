7cf8a650f8619b3ad2532e3d28008faa062877e0
Serafirim <serafirim@gmail.com>
2023-02-09 13:55:22 -0600

    push 1.0.1

---
+++ src/components/CharacterMap/CharacterMap.js
@@ -0,0 +1,49 @@
+import React, { memo, useMemo } from 'react'
+import PropTypes from 'prop-types'
+
+function itemize(text, transformer){
+    const letters = text.split('')
+      .filter(l => l !== ' ')
+      .reduce((collection, item) => {
+        const letter = transformer ? transformer(item) : item // Replaced: "const letter = item.toLowerCase()"
+        return {
+          ...collection,
+          [letter]: (collection[letter] || 0) + 1
+        }
+      }, {})
+    return Object.entries(letters) // Replaced: "return letters"
+     .sort((a, b) => b[1] - a[1])
+  }
+
+function CharacterMap({ showExplanation, text, transformer }) {
+  const characters = useMemo(() => itemize(text, transformer), [text, transformer])
+
+  return(
+    <div>
+        {showExplanation && 
+          <p>
+            This displays a list of most common characters.
+          </p>
+        }
+
+        Character Map:
+        {characters.map(character => (
+            <div key={character[0]}>
+            {character[0]}: {character[1]}
+            </div>
+        ))}
+    </div>
+)
+}
+
+CharacterMap.propTypes = {
+    showExplanation: PropTypes.bool.isRequired,
+    text: PropTypes.string.isRequired,
+    transformer: PropTypes.func
+}
+
+CharacterMap.defaultProps = {
+  transformer: null
+}
+
+export default memo(CharacterMap)
